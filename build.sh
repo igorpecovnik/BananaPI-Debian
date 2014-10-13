fa2e579234b76b36a7ba2c92a5672274d75e6fa6
Igor Pecovnik <igor.pecovnik@gmail.com>
2014-10-13 19:06:21 +0200

    no message

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Configuration 
 #--------------------------------------------------------------------------------------------------------------------------------
 
-BOARD="bananapi"						# bananapi, cubietruck, cubox-i, bananapi-next, cubietruck-next
+BOARD="bananapi"										# bananapi, cubietruck, cubox-i, bananapi-next, cubietruck-next
 RELEASE="wheezy"                                   		# jessie or wheezy
 VERSION="Banana Debian 1.3 $RELEASE"               		# just name
 SOURCE_COMPILE="yes"                               		# yes / no
@@ -13,30 +13,16 @@ DEST_LANG="en_US.UTF-8"                         	 	# sl_SI.UTF-8, en_US.UTF-8
 TZDATA="Europe/Ljubljana"                         		# Timezone
 DEST=$(pwd)/output                      		      	# Destination
 ROOTPWD="1234"                               		  	# Must be changed @first login
-HOST="banana"						   	# Hostname
-USEALLCORES="no"				 		# Use all CPU cores for compiling
-SDSIZE="1000"							# SD image size in MB
+HOST="banana"						  				 	# Hostname
+USEALLCORES="no"										# Use all CPU cores for compiling
+SDSIZE="1000"											# SD image size in MB
 
 #--------------------------------------------------------------------------------------------------------------------------------
-
 # superuser have to do this
 if [ "$UID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-
-# optimize build time with 100% CPU usage
-CPUS=$(grep -c 'processor' /proc/cpuinfo)
-if [ "$USEALLCORES" = "yes" ]; then
-CTHREADS="-j$(($CPUS + $CPUS/2))";
-else
-CTHREADS="-j${CPUS}";
-fi
-
-# to display build time at the end
-start=`date +%s`
-
-clear
 
 # source is where we start the script
 SRC=$(pwd)
@@ -50,69 +36,7 @@ else
 fi
 cd $SRC
 
-source $SRC/lib/common.sh # Functions
 source $SRC/lib/boards.config # Board configuration
-
-set -e
-
-echo "Building $VERSION."
-
+source $SRC/lib/common.sh # Functions
+source $SRC/lib/main.sh # Main
 #--------------------------------------------------------------------------------------------------------------------------------
-# Download packages for host
-#--------------------------------------------------------------------------------------------------------------------------------
-download_host_packages
-
-#--------------------------------------------------------------------------------------------------------------------------------
-# fetch_from_github [repository, sub directory]
-#--------------------------------------------------------------------------------------------------------------------------------
-mkdir -p $DEST/output
-fetch_from_github "$BOOTLOADER" "$BOOTSOURCE"
-fetch_from_github "$LINUXKERNEL" "$LINUXSOURCE"
-fetch_from_github "$DOCS" "$DOCSDIR"
-if [[ -n "$MISC1" ]]; then fetch_from_github "$MISC1" "$MISC1_DIR"; fi
-if [[ -n "$MISC2" ]]; then fetch_from_github "$MISC2" "$MISC2_DIR"; fi
-if [[ -n "$MISC3" ]]; then fetch_from_github "$MISC3" "$MISC3_DIR"; fi
-if [[ -n "$MISC4" ]]; then fetch_from_github "$MISC4" "$MISC4_DIR"; fi
-
-#--------------------------------------------------------------------------------------------------------------------------------
-# grab linux kernel version from Makefile
-#--------------------------------------------------------------------------------------------------------------------------------
-VER=$(cat $DEST/$LINUXSOURCE/Makefile | grep VERSION | head -1 | awk '{print $(NF)}')
-VER=$VER.$(cat $DEST/$LINUXSOURCE/Makefile | grep PATCHLEVEL | head -1 | awk '{print $(NF)}')
-VER=$VER.$(cat $DEST/$LINUXSOURCE/Makefile | grep SUBLEVEL | head -1 | awk '{print $(NF)}')
-EXTRAVERSION=$(cat $DEST/$LINUXSOURCE/Makefile | grep EXTRAVERSION | head -1 | awk '{print $(NF)}')
-if [ "$EXTRAVERSION" != "=" ]; then VER=$VER$EXTRAVERSION; fi
-
-
-
-if [ "$SOURCE_COMPILE" = "yes" ]; then
-#--------------------------------------------------------------------------------------------------------------------------------
-# Compiling everything
-#--------------------------------------------------------------------------------------------------------------------------------
-# Patching sources
-patching_sources
-# compile boot loader
-compile_uboot 
-# compile kernel and create archives
-compile_kernel
-# create tar file
-packing_kernel
-fi
-# prepare image with bootloader
-creating_image
-# install base system
-install_base_debian 
-# install additional applications
-install_applications
-# install board specific applications
-install_board_specific 
-# install external applications
-install_external_applications
-# add some summary to the image
-fingerprint_image "$DEST/output/sdcard/root/readme.txt"
-# closing image
-closing_image
-#
-end=`date +%s`
-runtime=$(((end-start)/60))
-echo "Runtime $runtime min."
