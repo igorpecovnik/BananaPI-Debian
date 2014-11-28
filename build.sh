118d9b4711225d9688a8ca4bc3eed6c414a7441d
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2014-11-28 11:33:24 +0100

    version update

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@
 BOARD="bananapi"						# bananapi
 BRANCH="default"						# default=3.4.x, mainline=next
 RELEASE="wheezy"                                   		# jessie or wheezy
-VERSION="Banana Debian 1.5 $RELEASE"               		# just name
+VERSION="Banana Debian 1.6 $RELEASE"               		# just name
 SOURCE_COMPILE="yes"                               		# yes / no
 KERNEL_CONFIGURE="yes"						# do you want to change my default configuration
 DEST_LANG="en_US.UTF-8"                         	 	# sl_SI.UTF-8, en_US.UTF-8
@@ -18,7 +18,7 @@ ROOTPWD="1234"                               		  	# Must be changed @first login
 HOST="banana"						 	# Hostname
 USEALLCORES="no"						# Use all CPU cores for compiling
 SDSIZE="1000"							# SD image size in MB
-FBTFT="no"							# Small TFT support, https://github.com/notro/fbtft
+FBTFT="yes"							# Small TFT support, https://github.com/notro/fbtft
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # superuser have to do this
