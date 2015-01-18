157299cce817a1a2a3018ad1aed92b6c1f2cc5ee
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2015-01-18 07:29:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,27 +10,27 @@ BRANCH="default"						# default=3.4.x, mainline=next
 RELEASE="wheezy"                                   		# jessie, wheezy or trusty
 
 # numbers
-SDSIZE="1200"                               # SD image size in MB
-REVISION="2.0"                              # image release version
+SDSIZE="1200"                               			# SD image size in MB
+REVISION="2.0"                          			# image release version
 
 # method
-SOURCE_COMPILE="no"                        # force source compilation: yes / no
-KERNEL_CONFIGURE="no"                       # want to change my default configuration
-KERNEL_CLEAN="no"                          # run MAKE clean before kernel compilation
-USEALLCORES="no"                           # Use all CPU cores for compiling
+SOURCE_COMPILE="yes"             				# force source compilation: yes / no
+KERNEL_CONFIGURE="no"           				# want to change my default configuration
+KERNEL_CLEAN="yes"						# run MAKE clean before kernel compilation
+USEALLCORES="yes"                           			# Use all CPU cores for compiling
 
 # user 
-DEST_LANG="en_US.UTF-8"                     # sl_SI.UTF-8, en_US.UTF-8
-TZDATA="Europe/Ljubljana"                   # Timezone
-ROOTPWD="1234"                              # Must be changed @first login
-MAINTAINER="Igor Pecovnik"                  # deb signature
-MAINTAINERMAIL="igor.pecovnik@****l.com"    # deb signature
+DEST_LANG="en_US.UTF-8"                     			# sl_SI.UTF-8, en_US.UTF-8
+TZDATA="Europe/Ljubljana"               			# Timezone
+ROOTPWD="1234"                              			# Must be changed @first login
+MAINTAINER="Igor Pecovnik"                  			# deb signature
+MAINTAINERMAIL="igor.pecovnik@****l.com"    			# deb signature
 
 # advanced
-FBTFT="yes"                                 # https://github.com/notro/fbtft 
-EXTERNAL="no"                              # compile extra drivers`
+FBTFT="no"                                 			# https://github.com/notro/fbtft 
+EXTERNAL="no"                           			# compile extra drivers`
 
-#---------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------
 
 # source is where we start the script
 SRC=$(pwd)
@@ -48,4 +48,4 @@ else
 fi
 
 source $SRC/lib/main.sh
-#---------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------
